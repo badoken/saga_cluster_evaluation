57f522859be6f26d32ed4625eba1b7ab7b7f77cd
badoken <badoken1101@gmail.com>
2019-11-24 11:25:50 +0300

    Summary distributed transaction display

--- bin/time.py
+++ bin/time.py
@@ -4,7 +4,6 @@ from functional import seq
 from matplotlib import pyplot
 
 from distributed_transaction_factory import DistributedTransactionFactory
-from transaction import Transaction
 
 distributed_transaction_factory = DistributedTransactionFactory()
 
@@ -12,41 +11,37 @@ distribute_transactions = [distributed_transaction_factory.generate(random.randi
 
 first_distributed_transaction = distribute_transactions[0]
 
-local_transaction_times_if_all_data_and_save_to_disc = list(map(
-    lambda transaction: transaction.get_time(is_save_to_disc=True, is_useful_data_only=False),
-    first_distributed_transaction.local_transactions))
-local_transaction_times_if_all_data_and_no_save_to_disc = list(map(
-    lambda transaction: transaction.get_time(is_save_to_disc=False, is_useful_data_only=False),
-    first_distributed_transaction.local_transactions))
-local_transaction_times_if_useful_data_and_save_to_disc = list(map(
-    lambda transaction: transaction.get_time(is_save_to_disc=True, is_useful_data_only=True),
-    first_distributed_transaction.local_transactions))
-local_transactions_time_if_useful_data_and_no_save_to_disc = list(map(
-    lambda transaction: transaction.get_time(is_save_to_disc=False, is_useful_data_only=True),
-    first_distributed_transaction.local_transactions))
+local_transaction_times = {
+    'all data with save to disc': list(map(
+        lambda transaction: transaction.get_time(is_save_to_disc=True, is_useful_data_only=False),
+        first_distributed_transaction.local_transactions)),
+    'useful data with save to disc': list(map(
+        lambda transaction: transaction.get_time(is_save_to_disc=True, is_useful_data_only=True),
+        first_distributed_transaction.local_transactions)),
+    'all data without save to disc': list(map(
+        lambda transaction: transaction.get_time(is_save_to_disc=False, is_useful_data_only=False),
+        first_distributed_transaction.local_transactions)),
+    'useful data without save to disc': list(map(
+        lambda transaction: transaction.get_time(is_save_to_disc=False, is_useful_data_only=True),
+        first_distributed_transaction.local_transactions))
+}
 
 figure1 = pyplot.figure(1)
 pyplot.title("Local transactions time of one distributed transaction")
-pyplot.plot(local_transaction_times_if_all_data_and_save_to_disc)
-pyplot.plot(local_transaction_times_if_all_data_and_no_save_to_disc)
-pyplot.plot(local_transaction_times_if_useful_data_and_save_to_disc)
-pyplot.plot(local_transactions_time_if_useful_data_and_no_save_to_disc)
-pyplot.legend(['all data with save to disc',
-               'all data without save to disc',
-               'useful data with save to disc',
-               'useful data without save to disc'],
-              loc='upper left')
+for transaction_time in local_transaction_times.values():
+    pyplot.plot(transaction_time)
+pyplot.legend(local_transaction_times.keys(), loc='upper left')
 pyplot.xlabel("local transaction number")
 pyplot.ylabel("time")
 
 distributed_transaction_times_if_all_data_and_save_to_disc = list(map(
     lambda d_transaction: d_transaction.summary_transaction_time(is_save_to_disc=True, is_useful_data_only=False),
     distribute_transactions))
-distributed_transaction_times_if_all_data_and_no_save_to_disc = list(map(
-    lambda d_transaction: d_transaction.summary_transaction_time(is_save_to_disc=False, is_useful_data_only=False),
-    distribute_transactions))
 distributed_transaction_times_if_useful_data_and_save_to_disc = list(map(
     lambda d_transaction: d_transaction.summary_transaction_time(is_save_to_disc=True, is_useful_data_only=True),
+    distribute_transactions))
+distributed_transaction_times_if_all_data_and_no_save_to_disc = list(map(
+    lambda d_transaction: d_transaction.summary_transaction_time(is_save_to_disc=False, is_useful_data_only=False),
     distribute_transactions))
 distributed_transaction_times_if_useful_data_and_no_save_to_disc = list(map(
     lambda d_transaction: d_transaction.summary_transaction_time(is_save_to_disc=False, is_useful_data_only=True),
@@ -55,42 +50,26 @@ distributed_transaction_times_if_useful_data_and_no_save_to_disc = list(map(
 figure2 = pyplot.figure(2)
 pyplot.title("Distributed transactions time")
 pyplot.plot(distributed_transaction_times_if_all_data_and_save_to_disc)
-pyplot.plot(distributed_transaction_times_if_all_data_and_no_save_to_disc)
 pyplot.plot(distributed_transaction_times_if_useful_data_and_save_to_disc)
+pyplot.plot(distributed_transaction_times_if_all_data_and_no_save_to_disc)
 pyplot.plot(distributed_transaction_times_if_useful_data_and_no_save_to_disc)
 pyplot.legend(['all data with save to disc',
-               'all data without save to disc',
                'useful data with save to disc',
+               'all data without save to disc',
                'useful data without save to disc'],
               loc='upper left')
 pyplot.xlabel("distributed transaction number")
 pyplot.ylabel("time")
 
-distributed_transactions_summary_time_if_all_data_and_save_to_disc = \
-    seq(distributed_transaction_times_if_all_data_and_save_to_disc).sum()
-distributed_transactions_summary_time_if_all_data_and_no_save_to_disc = seq(
-    distributed_transaction_times_if_all_data_and_no_save_to_disc).sum()
-distributed_transactions_summary_time_if_useful_data_and_save_to_disc = \
-    seq(distributed_transaction_times_if_useful_data_and_save_to_disc).sum()
-distributed_transactions_summary_time_if_useful_data_and_no_save_to_disc = \
-    seq(distributed_transaction_times_if_useful_data_and_no_save_to_disc).sum()
+distributed_transactions_summary = {
+    'all data with save to disc': seq(distributed_transaction_times_if_all_data_and_save_to_disc).sum(),
+    'useful data with save to disc': seq(distributed_transaction_times_if_useful_data_and_save_to_disc).sum(),
+    'all data without save to disc': seq(distributed_transaction_times_if_all_data_and_no_save_to_disc).sum(),
+    'useful data without save to disc': seq(distributed_transaction_times_if_useful_data_and_no_save_to_disc).sum()
+}
 
 figure3 = pyplot.figure(3)
-pyplot.title("Summary time required for " + str(len(distribute_transactions)) + "distributed transactions")
-print("summary times: " + str([distributed_transactions_summary_time_if_all_data_and_save_to_disc,
-                               distributed_transactions_summary_time_if_all_data_and_no_save_to_disc,
-                               distributed_transactions_summary_time_if_useful_data_and_save_to_disc,
-                               distributed_transactions_summary_time_if_useful_data_and_no_save_to_disc]))
-pyplot.hist([distributed_transactions_summary_time_if_all_data_and_save_to_disc,
-             distributed_transactions_summary_time_if_all_data_and_no_save_to_disc,
-             distributed_transactions_summary_time_if_useful_data_and_save_to_disc,
-             distributed_transactions_summary_time_if_useful_data_and_no_save_to_disc])
-# pyplot.legend(['all data with save to disc',
-#                'all data without save to disc',
-#                'useful data with save to disc',
-#                'useful data without save to disc'],
-#               loc='upper left')
-# pyplot.xlabel("distributed transaction number")
-# pyplot.ylabel("time")
+pyplot.title("Summary time required for " + str(len(distribute_transactions)) + " distributed transactions")
+pyplot.bar(x=distributed_transactions_summary.keys(), height=distributed_transactions_summary.values(), align='center')
 
 pyplot.show()
